a53770090ffa3dd3e013ce59bef6581622aae7bc
lk <lk@handverksdata.no>
2025-08-14 17:36:52 +0200

    seeding offers near oslo

--- internal/postgres/pg-global-offers.go
+++ internal/postgres/pg-global-offers.go
@@ -14,21 +14,45 @@ func NewPgGlobalOffers(DB *PgDb) offer.GlobalOffers {
 }
 
 func (globalOffers PgGlobalOffers) NearBy(radar *common.RadarModel) ([]offer.Offer, error) {
-	offers := make([]offer.Offer, 2)
-	offer1 := offer.NewSolidOffer(
-		nil,
-		&offer.OfferModel{
-			Price:    &common.PriceModel{Value: 120000, Currency: "NOK"},
-			Position: &common.PositionModel{},
-		},
-		1)
-	offer2 := offer.NewSolidOffer(
-		nil,
-		&offer.OfferModel{
-			Price:    &common.PriceModel{Value: 50099, Currency: "NOK"},
-			Position: &common.PositionModel{},
-		},
-		1)
-	offers = append(offers, offer1, offer2)
+	id := 0
+	query := "select * from offer"
+	offers := []offer.Offer{}
+	rows, err := globalOffers.DB.Database.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		addressRow := new(common.AddressModel)
+		priceRow := new(common.PriceModel)
+		positionRow := new(common.PositionModel)
+		err := rows.Scan(
+			&id,
+			&addressRow.Line1,
+			&addressRow.Line2,
+			&addressRow.City,
+			&addressRow.PostalCode,
+			&addressRow.District,
+			&positionRow.Lat,
+			&positionRow.Lon,
+			&priceRow.Value,
+			&priceRow.Currency,
+		)
+		pgOffer := PgOffer{DB: globalOffers.DB, ID: id}
+		if err != nil {
+			return nil, err
+		}
+
+		solidOffer := offer.NewSolidOffer(
+			pgOffer,
+			&offer.OfferModel{
+				Id:       id,
+				Address:  addressRow,
+				Price:    priceRow,
+				Position: positionRow,
+			},
+			id)
+		offers = append(offers, solidOffer)
+	}
 	return offers, nil
 }
